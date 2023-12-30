54eba089ffa940ea209b0b2adf6c48f5426c7390
JessCP25 <jpayanoc25@gmail.com>
2023-12-30 00:05:28 -0500

    configuracion del dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SidemenuComponent } from '../shared/sidemenu/sidemenu.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [
-    RouterModule
+    RouterModule,
+    SidemenuComponent
   ],
   templateUrl: './dashboard.component.html',
   styles: `
--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { routes } from '../../app.routes';
 
 @Component({
   selector: 'app-sidemenu',
   standalone: true,
   imports: [
     CommonModule,
+    RouterModule
   ],
   templateUrl: './sidemenu.component.html',
   styles: `
@@ -14,4 +17,10 @@ import { Component } from '@angular/core';
     }
   `,
 })
-export class SidemenuComponent { }
+export class SidemenuComponent {
+  public menuItems = routes
+    .map((route)=> route.children ?? [])
+    .flat()
+    .filter((route)=> route && route.path)
+    .filter((route)=> !route.path?.includes(':'));
+}
