c6289c0e1a7b20e74ddf8b8a67ac0e39e3baa39c
JessCP25 <jpayanoc25@gmail.com>
2023-12-30 19:40:52 -0500

    ChangeDetection - onPush

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,17 +1,46 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   selector: 'app-change-detection',
   standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
-    CommonModule,
+    CommonModule, TitleComponent
   ],
-  templateUrl: './change-detection.component.html',
+  template: `
+    <app-title [title]="this.currentFramework()"></app-title>
+    <pre> {{frameworkAsSignal() | json }} </pre>
+    <pre> {{ frameworkAsProperty | json}} </pre>
+  `,
   styles: `
     :host {
       display: block;
     }
   `,
 })
-export default class ChangeDetectionComponent { }
+export default class ChangeDetectionComponent {
+  public currentFramework = computed(
+    () => `Change Detection - ${this.frameworkAsSignal().name}`
+  )
+
+  public frameworkAsSignal = signal({
+    name: 'Angular',
+    releaseDate: 2016,
+  })
+
+  public frameworkAsProperty = {
+    name: 'Angular',
+    releaseDate: 2016
+  }
+
+  constructor(){
+    setTimeout(()=>{
+      this.frameworkAsSignal.update(name => ({
+        ...name,
+        name: 'React'
+      }))
+    }, 3000)
+  }
+}
